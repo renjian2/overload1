e43fa946365ebbf60eb230fec00d1e8f73182923
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-25 16:45:23 +0530

    Optimized Task-Search for upto 6% increase in Performance
    Signed-off-by: rishabhrao <admin@rishabhrao.net>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5231,6 +5231,7 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 	u64 ns = 0;
 
 #if defined(CONFIG_64BIT) && defined(CONFIG_SMP)
+<<<<<<< HEAD
 	/*
 	 * 64-bit doesn't need locks to atomically read a 64bit value.
 	 * So we have a optimization chance when the task's delta_exec is 0.
@@ -5242,6 +5243,19 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 	 */
 	if (!p->on_cpu)
 		return p->se.sum_exec_runtime;
+=======
+ /*
+	* 64-bit doesn't need locks to atomically read a 64bit value.
+	* So we have a optimization chance when the task's delta_exec is 0.
+	* Reading ->on_cpu is racy, but this is ok.
+	*
+	* If we race with it leaving cpu, we'll take a lock. So we're correct.
+	* If we race with it entering cpu, unaccounted time is 0. This is
+	* indistinguishable from the read occurring a few cycles earlier.
+	*/
+ if (!p->on_cpu)
+ return p->se.sum_exec_runtime;
+>>>>>>> 0d0b35f... Optimized Task-Search for upto 6% increase in Performance
 #endif
 
 	rq = task_rq_lock(p, &flags);
