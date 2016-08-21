c20f93fbf7b4849ce2f588ca3d854fdd85d360f6
Shoaib0597 <Shoaib0595@gmail.com>
2016-08-21 16:02:35 +0530

    Tune the Task-Scheduler for Power-Savings
    1. Enable HMP-Aware and Power-Aware Task-Scheduling.
    This will help in Saving More Power without any noticeable Performance-Hit. Further, this will also help the Scheduler to take Full Advantage of the big.LITTLE Architecture.
    
    2. Make the Task-Scheduler Utilize the LITTLE Cluster Aggressively.
    Alter the Task-Thresholds so that the Task-Scheduler uses LITTLE Cluster for most of the tasks in order to Save Power. Further, Android Background Processes have a Default Value of Min. Nice set to 10. Thus, Decrease the Min. Nice Value of Task-Scheduler to 9 in order to Prevent these Processes to be Scheduled on the big Cluster. This way, the load on the big Cluster gets reduced significantly, leading to Aggressive Power-Savings.
    
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1382,7 +1382,7 @@ unsigned int max_task_load(void)
 }
 
 /* Use this knob to turn on or off HMP-aware task placement logic */
-unsigned int __read_mostly sched_enable_hmp = 0;
+unsigned int __read_mostly sched_enable_hmp = 1;
 
 /* A cpu can no longer accomodate more tasks if:
  *
@@ -1396,7 +1396,11 @@ unsigned int __read_mostly sysctl_sched_spill_nr_run = 10;
  * guide task placement.
  * This sysctl can be set to a default value using boot command line arguments.
  */
+<<<<<<< HEAD
 unsigned int __read_mostly sysctl_sched_enable_power_aware = 0;
+=======
+unsigned int __read_mostly sched_enable_power_aware = 1;
+>>>>>>> 36995bf... Tune the Task-Scheduler for Power-Savings
 
 /*
  * This specifies the maximum percent power difference between 2
@@ -1419,7 +1423,7 @@ unsigned int __read_mostly sysctl_sched_spill_load_pct = 100;
  * sched_small_task are considered as small tasks.
  */
 unsigned int __read_mostly sched_small_task;
-unsigned int __read_mostly sysctl_sched_small_task_pct = 10;
+unsigned int __read_mostly sysctl_sched_small_task_pct = 65;
 
 /*
  * Tasks with demand >= sched_heavy_task will have their
@@ -1440,7 +1444,7 @@ unsigned int __read_mostly sched_heavy_task;
  * capacity.
  */
 unsigned int __read_mostly sched_upmigrate;
-unsigned int __read_mostly sysctl_sched_upmigrate_pct = 80;
+unsigned int __read_mostly sysctl_sched_upmigrate_pct = 95;
 
 /*
  * Big tasks, once migrated, will need to drop their bandwidth
@@ -1448,14 +1452,14 @@ unsigned int __read_mostly sysctl_sched_upmigrate_pct = 80;
  * migrated.
  */
 unsigned int __read_mostly sched_downmigrate;
-unsigned int __read_mostly sysctl_sched_downmigrate_pct = 60;
+unsigned int __read_mostly sysctl_sched_downmigrate_pct = 80;
 
 /*
  * Tasks whose nice value is > sysctl_sched_upmigrate_min_nice are never
  * considered as "big" tasks.
  */
-static int __read_mostly sched_upmigrate_min_nice = 15;
-int __read_mostly sysctl_sched_upmigrate_min_nice = 15;
+static int __read_mostly sched_upmigrate_min_nice = 9;
+int __read_mostly sysctl_sched_upmigrate_min_nice = 9;
 
 /* grp upmigrate/downmigrate */
 unsigned int __read_mostly sched_grp_upmigrate;
@@ -3133,7 +3137,11 @@ unsigned int cpu_temp(int cpu)
 
 #else	/* CONFIG_SCHED_HMP */
 
+<<<<<<< HEAD
 #define sysctl_sched_enable_power_aware 0
+=======
+#define sched_enable_power_aware 1
+>>>>>>> 36995bf... Tune the Task-Scheduler for Power-Savings
 
 static inline int task_will_fit(struct task_struct *p, int cpu)
 {
