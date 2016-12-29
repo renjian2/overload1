68c907fcdc6734f6fdbfc1323e05921f30a7a93e
Linus Torvalds <torvalds@linux-foundation.org>
2016-12-29 20:15:14 +0530

    Upstream Kernel From 3.10.95 to 3.10.104
    ALL CREDITS FOR MERGING THIS COMMIT GOES TO @DroidThug from "https://github.com/DroidThug"
    
    Change-Id: I73289a466e961eb0a0b7f5b4be0267ddb6b8e2f3
    Signed-off-by: rishabhrao <admin@rishabhrao.net>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -754,6 +754,8 @@ struct user_struct {
 #endif
 	unsigned long locked_shm; /* How many pages of mlocked shm ? */
 
+	unsigned long unix_inflight;	/* How many files in flight in unix sockets */
+
 #ifdef CONFIG_KEYS
 	struct key *uid_keyring;	/* UID specific keyring */
 	struct key *session_keyring;	/* UID's default session keyring */
