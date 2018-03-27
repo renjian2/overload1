0c4211fc93dce0fde6cf4a6efd7c593df2a442d2
renjian2 <renjian503@gmail.com>
2018-03-27 09:46:33 +0000

    Revert "CHROMIUM: cpuquiet: Export min/max CPUs to userspace"
    This reverts commit 265e0a017bc2ceae9a0a9154fb94e27df6831567.

--- drivers/cpuquiet/cpuquiet.c
+++ drivers/cpuquiet/cpuquiet.c
@@ -30,11 +30,7 @@
 #define DEFAULT_AVG_HOTPLUG_LATENCY_MS	2
 #define DEFAULT_HOTPLUG_TIMEOUT_MS	100
 
-unsigned int cpuquiet_nr_max_cpus;
-unsigned int cpuquiet_nr_min_cpus;
-
 static DEFINE_MUTEX(cpuquiet_cpu_lock);
-DEFINE_MUTEX(cpuquiet_min_max_cpus_lock);
 
 static bool cpuquiet_devices_initialized;
 
@@ -51,11 +47,6 @@ static struct cpumask cr_offline_requests;
 static struct platform_device *cpuquiet_pdev;
 
 static void cpuquiet_work_func(struct work_struct *work);
-
-void cpuquiet_queue_work(void)
-{
-	queue_work(cpuquiet_wq, &cpuquiet_work);
-}
 
 /**
  * update_core_config - queues the work of onlining/offlining cpus
@@ -157,19 +148,9 @@ static void __cpuinit cpuquiet_work_func(struct work_struct *work)
 	unsigned int cpu;
 	int nr_cpus;
 	struct cpumask online, offline, cpu_online;
-	int max_cpus, min_cpus;
-
-	mutex_lock(&cpuquiet_min_max_cpus_lock);
-
-	max_cpus = min_t(unsigned int,
-				pm_qos_request(PM_QOS_MAX_ONLINE_CPUS) ? :
-							num_present_cpus(),
-				cpuquiet_nr_max_cpus);
-	min_cpus = max_t(unsigned int,
-				pm_qos_request(PM_QOS_MIN_ONLINE_CPUS),
-				cpuquiet_nr_min_cpus);
-
-	mutex_unlock(&cpuquiet_min_max_cpus_lock);
+	int max_cpus = pm_qos_request(PM_QOS_MAX_ONLINE_CPUS) ? :
+				num_present_cpus();
+	int min_cpus = pm_qos_request(PM_QOS_MIN_ONLINE_CPUS);
 
 	mutex_lock(&cpuquiet_cpu_lock);
 
@@ -364,10 +345,6 @@ static int cpuquiet_probe(struct platform_device *pdev)
 		pr_err("Failed to register max cpus PM QoS notifier\n");
 		goto remove_min;
 	}
-
-	cpuquiet_nr_max_cpus = pm_qos_request(PM_QOS_MAX_ONLINE_CPUS) ? :
-							num_present_cpus();
-	cpuquiet_nr_min_cpus = pm_qos_request(PM_QOS_MIN_ONLINE_CPUS);
 
 	err = cpuquiet_register_devices();
 	if (err)
