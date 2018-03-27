b63bc0bfa1a6ac2f39383eee4209fce8b55a7e6f
renjian2 <renjian503@gmail.com>
2018-03-27 09:46:42 +0000

    Revert "CHROMIUM: cpuquiet: Add support for arch specific sampling rate."
    This reverts commit bd0cf493af0949d110432e3f22af6ba202c0a1da.

--- drivers/cpuquiet/cpuquiet-tegra.c
+++ drivers/cpuquiet/cpuquiet-tegra.c
@@ -28,7 +28,6 @@
 #include <linux/sched.h>
 
 #define DEFAULT_HOTPLUG_DELAY_MS	100
-#define TEGRA_AVG_HOTPLUG_LATENCY_MS	2
 
 static DEFINE_MUTEX(tegra_cpu_lock);
 
@@ -104,10 +103,9 @@ static int tegra_wake_cpu(unsigned int cpunumber, bool sync)
 }
 
 static struct cpuquiet_driver tegra_cpuquiet_driver = {
-	.name			= "tegra",
-	.quiesce_cpu		= tegra_quiesce_cpu,
-	.wake_cpu		= tegra_wake_cpu,
-	.avg_hotplug_latency_ms	= TEGRA_AVG_HOTPLUG_LATENCY_US,
+	.name		= "tegra",
+	.quiesce_cpu	= tegra_quiesce_cpu,
+	.wake_cpu	= tegra_wake_cpu,
 };
 
 static void __cpuinit tegra_cpuquiet_work_func(struct work_struct *work)
